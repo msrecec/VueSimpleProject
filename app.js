17fbb6ee316f1678c8a0c8de6f6437cd4dd2bbbf
Mislav Srecec <mislav.srecec3@gmail.com>
2020-09-23 20:44:36 +0200

    Classes and Computed Properties

--- app.js
+++ app.js
@@ -6,14 +6,25 @@ const app = Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses() {
+      return { active: this.boxASelected };
+    },
+    boxBClasses() {
+      return { active: this.boxBSelected };
+    },
+    boxCClasses() {
+      return { active: this.boxCSelected };
+    },
+  },
   methods: {
     boxSelected(box) {
       if(box === 'A') {
-        this.boxASelected = true;
+        this.boxASelected = !this.boxASelected;
       } else if(box === 'B') {
-        this.boxBSelected = true;
+        this.boxBSelected = !this.boxBSelected;
       } else {
-        this.boxBSelected = true;
+        this.boxCSelected = !this.boxCSelected;
       }
     }
   }
