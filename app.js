ee6558be76b802f8f90d4c6c52bb0a198548ff9f
Mislav Srecec <mislav.srecec3@gmail.com>
2020-09-24 18:56:22 +0200

    Lists and Keys

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       enteredGoalValue: '',
-      goals: []
+      goals: [],
     };
   },
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue);
-    }
-  }
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
+    },
+  },
 });
 
 app.mount('#user-goals');
